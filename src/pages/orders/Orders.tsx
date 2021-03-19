d63b051648f0d61374602b476d862480593b22b2
ml <mlomzynski94@gmail.com>
2021-03-19 22:28:22 +0100

    add a button and validation action

--- src/pages/orders/Orders.tsx
+++ src/pages/orders/Orders.tsx
@@ -4,13 +4,16 @@ import { useOrderContext } from '../../context/Order.Context';
 interface IOrdersProps {}
 
 const Orders: React.FunctionComponent<IOrdersProps> = () => {
-  const { grandTotalPrice } = useOrderContext();
+  const { grandTotalPrice, isOrderValid } = useOrderContext();
   return (
     <>
       <Order type='scoops' />
       <Order type='toppings' />
       <div>
         <h2>Suma: {grandTotalPrice}</h2>
+      </div>
+      <div>
+        <button disabled={!isOrderValid}>Dalej</button>
       </div>
     </>
   );
