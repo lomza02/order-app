955b6a3f2a8c61a49d22d97a312731aa91f607c8
ml <mlomzynski94@gmail.com>
2021-03-19 13:44:20 +0100

    test and code of incorrect values in input

--- src/pages/orders/OrderInput.tsx
+++ src/pages/orders/OrderInput.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { IItem } from '../../models/IItem.model';
 import { useOrderContext } from '../../context/Order.Context';
 
@@ -13,15 +13,22 @@ const OrderInput: React.FunctionComponent<IOrderInputProps> = ({
   name,
   items,
 }) => {
+  const [errorText, setErrorText] = useState('');
   const { updateItems } = useOrderContext();
   const handleInput = (e: React.FormEvent<HTMLInputElement>) => {
-    const name = e.currentTarget.id;
-    const price = items.find((item: IItem) => item.name === name).price;
-    const amount =
+    const name: string = e.currentTarget.id;
+    const price: number = items.find((item: IItem) => item.name === name).price;
+    const amount: number =
       type === 'scoops'
         ? e.currentTarget.value
         : Number(e.currentTarget.checked);
+    if (amount < 0) {
+      return setErrorText('Ilość musi być większa niż zero');
+    } else if (amount % 1 !== 0) {
+      return setErrorText('Ilość musi być liczbą całkowitą');
+    }
     updateItems(name, price, amount, type);
+    setErrorText('');
   };
   return (
     <>
@@ -33,6 +40,7 @@ const OrderInput: React.FunctionComponent<IOrderInputProps> = ({
         onChange={handleInput}
         id={name}
       />
+      {errorText ? <p role='alert'>{errorText}</p> : null}
     </>
   );
 };
---
+++ src/pages/orders/tests/Input.test.tsx
@@ -0,0 +1,40 @@
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Order from '../Order';
+import { OrderProvider } from '../../../context/Order.Context';
+
+test('scoops input validation', async () => {
+  render(<Order type='scoops' />, { wrapper: OrderProvider });
+  const scoopChocolateInput = await screen.findByRole('spinbutton', {
+    name: /Smak czekoladowy/i,
+  });
+  //if amount is < 0
+  userEvent.clear(scoopChocolateInput);
+  userEvent.type(scoopChocolateInput, '-1');
+  expect(
+    screen.getByText('Ilość musi być większa niż zero')
+  ).toBeInTheDocument();
+  expect(
+    screen.queryByText('Ilość musi być liczbą całkowitą')
+  ).not.toBeInTheDocument();
+
+  //if amount is not intiger number
+  userEvent.clear(scoopChocolateInput);
+  userEvent.type(scoopChocolateInput, '1.1');
+  expect(
+    screen.getByText('Ilość musi być liczbą całkowitą')
+  ).toBeInTheDocument();
+  expect(
+    screen.queryByText('Ilość musi być większa niż zero')
+  ).not.toBeInTheDocument();
+
+  //if value is valid errors are hidden
+  userEvent.clear(scoopChocolateInput);
+  userEvent.type(scoopChocolateInput, '1');
+  expect(
+    screen.queryByText('Ilość musi być większa niż zero')
+  ).not.toBeInTheDocument();
+  expect(
+    screen.queryByText('Ilość musi być liczbą całkowitą')
+  ).not.toBeInTheDocument();
+});
