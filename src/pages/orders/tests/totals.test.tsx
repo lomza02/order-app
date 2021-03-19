44857188c6d5d952aa22acf2cbb668a255adb08f
ml <mlomzynski94@gmail.com>
2021-03-19 22:26:00 +0100

    fix the problem with act(...)

--- src/pages/orders/tests/totals.test.tsx
+++ src/pages/orders/tests/totals.test.tsx
@@ -1,11 +1,15 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Orders from '../Orders';
 import Order from '../Order';
 import { OrderProvider } from '../../../context/Order.Context';
 
 test('grand total', async () => {
-  render(<Orders />, { wrapper: OrderProvider });
+  const promise = Promise.resolve();
+  await act(async () => {
+    render(<Orders />, { wrapper: OrderProvider });
+    await promise;
+  });
   const chocolateInput = await screen.findByRole('spinbutton', {
     name: /Smak czekoladowy/i,
   });
@@ -25,20 +29,14 @@ test('grand total', async () => {
   userEvent.type(vanillaInput, '2');
   userEvent.click(cherryCheckbox);
   userEvent.click(mMsCheckbox);
+  const grandTotalDisplay = screen.getByText('Suma: ', { exact: false });
+  expect(grandTotalDisplay).toHaveTextContent('Suma: 8,00 zł');
 
-  await waitFor(() => {
-    const totalPriceDisplay = screen.getByText('Suma: 8,00 zł');
-    expect(totalPriceDisplay).toBeInTheDocument();
-  });
   userEvent.clear(chocolateInput);
   userEvent.clear(vanillaInput);
   userEvent.click(cherryCheckbox);
   userEvent.click(mMsCheckbox);
-
-  await waitFor(() => {
-    const totalPriceDisplay = screen.getByText('Suma: 0,00 zł');
-    expect(totalPriceDisplay).toBeInTheDocument();
-  });
+  expect(grandTotalDisplay).toHaveTextContent('Suma: 0,00 zł');
 });
 
 test('scoops subtotals', async () => {
@@ -49,23 +47,23 @@ test('scoops subtotals', async () => {
   const vanillaInput = await screen.findByRole('spinbutton', {
     name: /Smak waniliowy/i,
   });
-  const subTotDisplay = screen.getByText('Cena lodów: ', { exact: false });
+  const subtotalDisplay = screen.getByText('Cena lodów: ', { exact: false });
   //defult 0 zł
-  expect(subTotDisplay).toHaveTextContent('Cena lodów: 0,00 zł');
+  expect(subtotalDisplay).toHaveTextContent('Cena lodów: 0,00 zł');
   // 1 piece x 2 zł
   userEvent.clear(chocolateInput);
   userEvent.type(chocolateInput, '1');
-  expect(subTotDisplay).toHaveTextContent('Cena lodów: 2,00 zł');
+  expect(subtotalDisplay).toHaveTextContent('Cena lodów: 2,00 zł');
   // 4 pieces x 2 zł
   userEvent.clear(chocolateInput);
   userEvent.type(chocolateInput, '2');
   userEvent.clear(vanillaInput);
   userEvent.type(vanillaInput, '2');
-  expect(subTotDisplay).toHaveTextContent('Cena lodów: 8,00 zł');
+  expect(subtotalDisplay).toHaveTextContent('Cena lodów: 8,00 zł');
   // clear input = 0 pieces
   userEvent.clear(vanillaInput);
   userEvent.clear(chocolateInput);
-  expect(subTotDisplay).toHaveTextContent('Cena lodów: 0,00 zł');
+  expect(subtotalDisplay).toHaveTextContent('Cena lodów: 0,00 zł');
 });
 
 test('toppings subtotals', async () => {
