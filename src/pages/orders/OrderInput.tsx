6848a90428cb0938b5b985b8a0464d01b4c63aa6
ml <mlomzynski94@gmail.com>
2021-03-20 00:51:57 +0100

    OrderInput Component refactoring

--- src/pages/orders/OrderInput.tsx
+++ src/pages/orders/OrderInput.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import { IItem } from '../../models/IItem.model';
 import { useOrderContext } from '../../context/Order.Context';
 import { Offer } from '../../models/Offer.model';
-
 interface IOrderInputProps {
   type: Offer;
   name: string;
@@ -25,13 +24,28 @@ const OrderInput: React.FunctionComponent<IOrderInputProps> = ({
         type === 'scoops'
           ? +e.currentTarget.value
           : Number(e.currentTarget.checked);
-      if (amount < 0) {
-        return setErrorText('Ilość musi być większa niż zero');
-      }
-      if (amount % 1 !== 0) {
-        return setErrorText('Ilość musi być liczbą całkowitą');
-      }
-      if (updateItems) {
+      if ((amount && updateItems) || (amount === 0 && updateItems)) {
+        //condtions
+        const rules: {
+          condition: boolean;
+          msg: string;
+        }[] = [
+          { condition: amount < 0, msg: 'Ilość nie może być liczbą ujemną' },
+          { condition: amount === 0, msg: 'Ilość nie może być zerem' },
+          {
+            condition: amount % 1 !== 0,
+            msg: 'Ilość musi być liczbą całkowitą',
+          },
+        ];
+        //validation
+        for (const rule of rules) {
+          if (rule.condition) {
+            e.currentTarget.value = '';
+            updateItems(name, price, 0, type);
+            setErrorText('');
+            return setErrorText(rule.msg);
+          }
+        }
         updateItems(name, price, amount, type);
         setErrorText('');
       }
