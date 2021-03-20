7b9f7c31d3426e31b23187df28828473c79ebd60
ml <mlomzynski94@gmail.com>
2021-03-20 22:52:44 +0100

    add switching components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import Orders from './pages/orders/Orders';
+import Summary from './pages/summary/Summary';
+import Confirmation from './pages/confirmation/Confirmation';
+import { useState } from 'react';
 import { OrderProvider } from './context/Order.Context';
 
 const App = () => {
+  const [activePage, setActivePage] = useState('orders');
+  const handlePage = (route: string) => {
+    setActivePage(route);
+  };
+  function renderSwitch(page: string) {
+    switch (page) {
+      case 'orders':
+        return <Orders handlePage={handlePage} />;
+      case 'summary':
+        return <Summary handlePage={handlePage} />;
+      case 'confirmation':
+        return <Confirmation handlePage={handlePage} />;
+      default:
+        return <div>Nie znaleziono strony</div>;
+    }
+  }
   return (
     <div className='App'>
-      <OrderProvider>
-        <Orders />
-      </OrderProvider>
+      <OrderProvider>{renderSwitch(activePage)}</OrderProvider>
     </div>
   );
 };
