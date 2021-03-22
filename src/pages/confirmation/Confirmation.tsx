5d26f14069fa859b95507a556746de48922e6f4a
ml <mlomzynski94@gmail.com>
2021-03-22 16:42:54 +0100

    styling confirmation page

--- src/pages/confirmation/Confirmation.tsx
+++ src/pages/confirmation/Confirmation.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useOrderContext } from '../../context/Order.Context';
+import Button from '../common/components/Button';
+import './Confirmation.scss';
 
 interface IConfirmationProps {
   handlePage: (route: string) => any;
@@ -9,17 +11,21 @@ const Confirmation: React.FunctionComponent<IConfirmationProps> = ({
   handlePage,
 }) => {
   const [error, setError] = useState<boolean>(false);
-  const [code, setCode] = useState();
+  const [code, setCode] = useState('ładowanie...');
   const { clearItems } = useOrderContext();
+
   useEffect(() => {
     (async function fetchData() {
       try {
-        const res = await fetch('http://localhost:3030/order', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        });
+        const res = await fetch(
+          (process.env.REACT_APP_BACKEND as string) + '/order',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          }
+        );
         const parseRes = await res.json();
         setCode(parseRes.orderNumber);
       } catch {
@@ -37,9 +43,11 @@ const Confirmation: React.FunctionComponent<IConfirmationProps> = ({
     return <div>Wystąpił błąd podczas pobierania danych</div>;
   }
   return (
-    <div>
-      <div>Twój numer zamówienia: {code}</div>
-      <button onClick={handleBtn}>Wróć do zakupów</button>
+    <div className='confirmation'>
+      <div className='confirmation__information'>
+        Twój numer zamówienia: {code}
+      </div>
+      <Button onClick={handleBtn}>Wróć do zakupów</Button>
     </div>
   );
 };
